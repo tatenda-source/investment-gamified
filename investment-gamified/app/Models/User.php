b2425650519021fb259d222c106192e5b9533111
muudzo <tatendawalter62@gmail.com>
2025-11-27 18:24:50 +0800

    fix: add HasFactory trait to User model to enable database seeding

--- investment-gamified/app/Models/User.php
+++ investment-gamified/app/Models/User.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Authenticatable
 {
     use HasApiTokens, Notifiable;
+    use HasFactory, Notifiable;
 
     protected $fillable = [
         'name',
